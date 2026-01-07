97ebf7a0596722c9ff38a4bc21bfb41cb3a548fe
Munasinghe123 <jsan39196.com>
2026-01-07 15:48:08 +0530

    sprint one complete

--- Backend/Cron/PaymentCheck.js
+++ Backend/Cron/PaymentCheck.js
@@ -1,2 +1,18 @@
 
-const {checkPaymentStatus} = require('../Controllers/AdminController'); 
+console.log(">>> PaymentCheck.js LOADED <<<");
+
+const cron = require('node-cron');
+
+console.log("cron value:", cron);
+
+const { checkPaymentStatus } = require('../Controllers/AdminController');
+
+
+function startPaymentCheck() {
+    cron.schedule("*/1 * * * *", async () => {
+        console.log("Running payment status check...");
+        await checkPaymentStatus();
+    });
+}
+module.exports = {startPaymentCheck}
+
--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,18 @@
-
-const paymentCheckRoutes = require('./Routes/AdminRoutes');
-require('./Cron/PaymentCheck');
-const express = require('express');
 require('dotenv').config();
+const paymentCheckRoutes = require('./Routes/AdminRoutes');
+const express = require('express');
+const cors = require('cors');
+
+//corn job exported
+const{startPaymentCheck} = require('./Cron/PaymentCheck');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+startPaymentCheck();
 
 app.use('/api/payment/', paymentCheckRoutes);
 
